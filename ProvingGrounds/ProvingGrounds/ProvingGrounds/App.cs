41b630813d57a3124e34f4c635235bd136d869d6
Nicholas Busby <Nicholas.Busby@Live.com>
2016-02-02 20:54:51 -0500

    learning some stuff

---
+++ ProvingGrounds/ProvingGrounds/ProvingGrounds/App.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ProvingGrounds
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new Home();
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
